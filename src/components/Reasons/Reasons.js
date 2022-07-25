d1827e3159b6382ad1045e070b6fefeec2fcdd01
zeadhasan <zeadhasan1995shuvo@gmail.com>
2022-07-26 00:50:39 +0600

    reasons part done commit

--- src/components/Reasons/Reasons.js
+++ src/components/Reasons/Reasons.js
@@ -20,7 +20,44 @@ const Reasons = () => {
       </div>
 
       <div className="right-r">
-       
+        <span>SOME REASONS</span>
+        <div>
+          <span className="stroke-text">WHY </span>
+          <span>CHOOSE US?</span>
+        </div>
+
+        <div className="details-r">
+          <div>
+            <img src={tick} alt="" />
+            <span>OVER 140+ EXPERT COACHS</span>
+          </div>
+          <div>
+            <img src={tick} alt="" />
+            <span>TRAIN SMARTER AND FASTER THAN BEFORE</span>
+          </div>
+          <div>
+            <img src={tick} alt="" />
+            <span>1 FREE PROGRAM FOR NEW MEMBER</span>
+          </div>
+          <div>
+            <img src={tick} alt="" />
+            <span>RELIABLE PARTNERS</span>
+          </div>
+        </div>
+
+        <span
+          style={{
+            color: "var(--gray)",
+            fontWeight: "normal",
+          }}
+        >
+          OUR PARTNERS
+        </span>
+        <div className="partners">
+          <img src={nb} alt="" />
+          <img src={adidas} alt="" />
+          <img src={nike} alt="" />
+        </div>
       </div>
     </div>
   );
