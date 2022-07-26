dfbc7a7c8c8ebcdb039c5e7173320b9f84b5e273
zeadhasan <zeadhasan1995shuvo@gmail.com>
2022-07-26 21:30:19 +0600

    footer part commit

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,12 +1,26 @@
-import React from 'react';
-import './Footer.css';
+import React from "react";
+import "./Footer.css";
+import Github from "../../assets/github.png";
+import Instagram from "../../assets/instagram.png";
+import LinkedIn from "../../assets/linkedin.png";
+import logo from "../../assets/logo.png";
 
 const Footer = () => {
-    return (
-        <div>
-            
+  return (
+    <div className="Footer-container">
+      <hr />
+      <div className="footer">
+        <div className="social-link">
+          <img src={Github} alt="" />
+          <img src={Instagram} alt="" />
+          <img src={LinkedIn} alt="" />
         </div>
-    );
+        <div className="logo-f">
+          <img src={logo} alt="" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
