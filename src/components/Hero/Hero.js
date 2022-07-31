6a1ecfa7a62a8da0dac36c9e7923ae58deb207c9
zeadhasan <zeadhasan1995shuvo@gmail.com>
2022-07-31 17:02:52 +0600

    make hero Responsive commit

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -9,6 +9,7 @@ import { motion } from "framer-motion";
 
 const Hero = () => {
   const transition = { type: "spring", duration: 3 };
+  const mobile = window.innerWidth <= 768 ? true : false;
   return (
     <div className="hero">
       <div className="blur hero-blur"></div>
@@ -18,7 +19,7 @@ const Hero = () => {
         {/* the-best-ad part start */}
         <div className="the-best-ad">
           <motion.div
-            initial={{ left: "238px" }}
+            initial={{ left: mobile ? "165px" : "238px" }}
             whileInView={{ left: "8px" }}
             transition={{ ...transition, type: "tween" }}
           ></motion.div>
@@ -82,10 +83,13 @@ const Hero = () => {
         {/* hero image */}
         <img className="hero-image" src={hero_image} alt="" />
         <motion.img
-         initial={{ right: "11rem" }}
-         whileInView={{ right: "20rem" }}
-         transition={transition}
-         className="hero-image-back" src={hero_image_back} alt="" />
+          initial={{ right: "11rem" }}
+          whileInView={{ right: "20rem" }}
+          transition={transition}
+          className="hero-image-back"
+          src={hero_image_back}
+          alt=""
+        />
 
         {/* Calories start */}
         <motion.div
